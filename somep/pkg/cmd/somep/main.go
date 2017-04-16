b46c2fe0db7ab6faed635823aaa696b83028aa92
Jim Lin <jimlintw922@gmail.com>
2017-04-16 14:46:45 +0800

    update pkg/cmd/somep/main.go

--- somep/pkg/cmd/somep/main.go
+++ somep/pkg/cmd/somep/main.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    _ "crypto/md5"
+    _ "crypto/sha1"
+    _ "encoding/base64"
+    _ "encoding/csv"
+    _ "encoding/json"
+    _ "errors"
+    _ "flag"
+    _ "log"
+    _ "math/rand"
+    _ "net/url"
+    _ "os"
+    _ "os/exec"
+    _ "os/signal"
+    _ "regexp"
+    _ "syscall"
+    _ "time"
+)
 
 func main() {
 	server()
