d6a3362b73415d21192eed4e5c1d27f780ef4dd0
Jim Lin <jimlintw922@gmail.com>
2018-03-21 16:05:12 +0800

    mod fs/main.go

--- fs/main.go
+++ fs/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 )
+
+func copyFile(src, dst string) error {
+	// Open the source file for reading
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	// Open the destination file for writing
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	// Copy the contents of the source file into the destination file
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+
+	// Return any errors that result from closing the destination file
+	// Will return nil if no errors occurred
+	return d.Close()
+}
 
 func removeContents(dir string) error {
 	d, err := os.Open(dir)
