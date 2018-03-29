5b3a31f65f0bc0f1b916f435bf2f517b9b03c80d
Jim Lin <jimlintw922@gmail.com>
2018-03-29 16:41:14 +0800

    mod main.go

--- kafka/prosumer/README.md
+++ kafka/prosumer/README.md
@@ -6,6 +6,23 @@ brew install librdkafka
 go get -u github.com/confluentinc/confluent-kafka-go/kafka
 ```
 
+# Usage
+
+```
+# terminal 1 - monitor topic bar
+./kafka-console-consumer.sh --bootstrap-server 192.168.240.86:9092 --topic bar --from-beginning
+
+# terminal 2 - consume message from topic foo then produce message to topic bar
+go run main.go
+
+# terminal 3 - produce message to topic foo
+pip install kafka-python
+python
+>>> from kafka import KafkaProducer
+>>> producer = KafkaProducer(bootstrap_servers='192.168.240.86:9092')
+>>> producer.send('foo', b'1')
+```
+
 # Reference
 
 * confluent-kafka-go https://github.com/confluentinc/confluent-kafka-go/
--- kafka/prosumer/main.go
+++ kafka/prosumer/main.go
@@ -1,72 +1,125 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "github.com/confluentinc/confluent-kafka-go/kafka"
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func consumer() {
 
-    c, err := kafka.NewConsumer(&kafka.ConfigMap{
-        "bootstrap.servers": "localhost",
-        "group.id":          "myGroup",
-        "auto.offset.reset": "earliest",
-    })
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
 
-    for {
-        msg, err := c.ReadMessage(-1)
-        if err == nil {
-            fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-        } else {
-            fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-            break
-        }
-    }
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			break
+		}
+	}
 
-    c.Close()
+	c.Close()
 }
 
 func producer() {
 
-    p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
-    if err != nil {
-        panic(err)
-    }
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
 
-    // Delivery report handler for produced messages
-    go func() {
-        for e := range p.Events() {
-            switch ev := e.(type) {
-            case *kafka.Message:
-                if ev.TopicPartition.Error != nil {
-                    fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-                } else {
-                    fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-                }
-            }
-        }
-    }()
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
 
-    // Produce messages to topic (asynchronously)
-    topic := "myTopic"
-    for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-        p.Produce(&kafka.Message{
-            TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-            Value:          []byte(word),
-        }, nil)
-    }
+	// Produce messages to topic (asynchronously)
+	topic := "myTopic"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
 
-    // Wait for message deliveries
-    p.Flush(15 * 1000)
+	// Wait for message deliveries
+	p.Flush(15 * 1000)
 }
 
 func main() {
-    log.Println("TODO")
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "192.168.240.86",
+		"group.id":          "cclin",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer c.Close()
+
+	err = c.Subscribe("foo", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "192.168.240.86"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer p.Close()
+
+	//
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+
+			bar := "bar"
+			err := p.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &bar, Partition: kafka.PartitionAny},
+				Value:          msg.Value,
+			}, nil)
+
+			if err != nil {
+				log.Fatalf("Producer error: %v (%v)\n", err, msg)
+			}
+
+			break // just read one
+		} else {
+			log.Fatalf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	// Wait for message deliveries
+	p.Flush(15 * 1000)
+
+	log.Println("End")
 }
